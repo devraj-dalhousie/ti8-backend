7818011f2f16f19c49301dce9966a387b496fda2
dv247596@dal.ca <devrajvit@gmail.com>
2022-04-05 16:50:38 +0530

    Backend project setup

---
+++ src/main/java/com/dal/diet/Main.java
@@ -0,0 +1,11 @@
+package com.dal.diet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
