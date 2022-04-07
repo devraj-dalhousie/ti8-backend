db9c438791a5f1ad8b0f37783f3051c9f46550b9
dv247596@dal.ca <devrajvit@gmail.com>
2022-04-07 21:35:31 +0530

    Added cors annotation

--- src/main/java/com/dal/diet/controller/ProfileController.java
+++ src/main/java/com/dal/diet/controller/ProfileController.java
@@ -8,15 +8,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/profile")
 public class ProfileController {
 
